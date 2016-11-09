a81889ac8905d8cdb4f592d7584f3733e69a6a2d
pjbal <patrickjbalcombe@hotmail.com>
2016-11-09 23:12:57 +0000

    n nit mux bits

---
+++ TB_TwoInputMultiplexor_VHDL.vhd
@@ -0,0 +1,94 @@
+--------------------------------------------------------------------------------
+-- Company: QMUL DSD Group
+-- Engineer: Patrick Balcombe
+--
+-- Create Date:   21:17:03 10/09/2016
+-- Design Name:   Test bench for TwoInputMultiplexor
+-- Module Name:   H:/Documents/DSD/LAB1/DSD_LAB1_TwoInputMultiplexor/TB_TwoInputMultiplexor_VHDL.vhd
+-- Project Name:  DSD_LAB1_TwoInputMultiplexor
+-- Target Device:  
+-- Tool versions:  
+-- Description:   Test Bench to go through all possible inputs. 
+-- 
+-- VHDL Test Bench Created by ISE for module: TwoInputMultiplexor_VHDL
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY TB_TwoInputMultiplexor_VHDL IS
+END TB_TwoInputMultiplexor_VHDL;
+ 
+ARCHITECTURE behavior OF TB_TwoInputMultiplexor_VHDL IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT TwoInputMultiplexor_VHDL
+    PORT(
+         a : IN  std_logic;
+         b : IN  std_logic;
+         Con : IN  std_logic;
+         d : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal sig_a : std_logic := '0';
+   signal sig_b : std_logic := '0';
+   signal Con : std_logic := '0';
+
+ 	--Outputs
+   signal sig_d : std_logic;
+
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: TwoInputMultiplexor_VHDL PORT MAP (sig_a, sig_b, Con, sig_d);
+
+
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+		--loop through all combinations of input signals
+      floop1: for i in std_logic range '0' to '1' loop
+			floop2: for j in std_logic range '0' to '1' loop
+				floop3: for k in std_logic range '0' to '1' loop
+			
+					sig_a <= k;
+					sig_b <= j;
+					Con <= i;
+				
+					wait for 100ns;
+					
+				end loop;
+			end loop;
+		end loop;  
+		--end of loop through input signals
+
+      wait;
+   end process;
+
+END;
---
+++ TB_nbitTwoInputMux_VHDL.vhd
@@ -0,0 +1,93 @@
+--------------------------------------------------------------------------------
+-- Company: QMUL DSD Group 2
+-- Engineer: Patrick Balcombe
+--
+-- Create Date:   22:29:06 10/20/2016
+-- Design Name:   Test bench n-bit two input Mux
+-- Module Name:   H:/Documents/DSD/LAB2/nbitTwoInputMux/TB_nbitTwoInputMux_VHDL.vhd
+-- Project Name:  nbitTwoInputMux
+-- Target Device:  
+-- Tool versions:  
+-- Description:   Test bench to test all possible input to n-bit two input Mux
+-- 
+-- VHDL Test Bench Created by ISE for module: nbitTwoInputMux_VHDL
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+ 
+ENTITY TB_nbitTwoInputMux_VHDL IS
+END TB_nbitTwoInputMux_VHDL;
+ 
+ARCHITECTURE behavior OF TB_nbitTwoInputMux_VHDL IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT nbitTwoInputMux_VHDL
+    PORT(
+         InA : IN  std_logic_vector(3 downto 0);
+         InB : IN  std_logic_vector(3 downto 0);
+         Control : IN  std_logic;
+         Output : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal InA : std_logic_vector(3 downto 0) := (others => '0');
+   signal InB : std_logic_vector(3 downto 0) := (others => '0');
+   signal Control : std_logic := '0';
+
+ 	--Outputs
+   signal Output : std_logic_vector(3 downto 0);
+   
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: nbitTwoInputMux_VHDL PORT MAP (
+          InA => InA,
+          InB => InB,
+          Control => Control,
+          Output => Output
+        );
+
+  
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+       loop0: for i in 15 downto 0 loop --loop through all combinations of InA
+			loop1: for j  in 15 downto 0  loop --loop through all combinations of InB
+				loop2: for k  in std_logic range '0' to '1'  loop --Use both posiible values of C_Term					
+								
+					--assign new combination
+					InA <= std_logic_vector(to_unsigned(i, 4));
+					InB <= std_logic_vector(to_unsigned(j, 4));
+					Control <= k;
+								
+					wait for 100ns;--wait to see responce		
+					
+				end loop;
+			end loop; 
+		end loop;
+
+      wait;
+   end process;
+
+END;
